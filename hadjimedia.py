dc23e8f253cdcf749635418455e6aaf0d79d70a0
HadjiMedia <154067103+HadjiMedia@users.noreply.github.com>
2025-09-12 13:17:43 +0800

    Update hadjimedia.py

--- hadjimedia.py
+++ hadjimedia.py
@@ -1,6 +1,6 @@
 import requests
 
-__ENDPOINT_URL__: str = "https://hadjitermux.squareweb.app/api"
+__ENDPOINT_URL__: str = "https://hadjitermux.squareweb.app/application/api"
 
 class HadjiMedia:
     def __init__(self, access_key) -> None:
